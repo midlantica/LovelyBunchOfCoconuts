c2ad656c01c7467975f1f0ad120f1f2681464272
Drew Harper <druharper@gmail.com>
2025-06-25 12:27:13 -0500

    save

---
+++ .nuxt/content/manifest.ts
@@ -0,0 +1,61 @@
+export const checksums = {}
+export const checksumsStructure = {}
+
+export const tables = {
+  "claims": "_content_claims",
+  "quotes": "_content_quotes",
+  "memes": "_content_memes",
+  "info": "_content_info"
+}
+
+export default {
+  "claims": {
+    "type": "page",
+    "fields": {
+      "id": "string",
+      "title": "string",
+      "body": "json",
+      "description": "string",
+      "extension": "string",
+      "meta": "json",
+      "navigation": "json",
+      "path": "string",
+      "seo": "json",
+      "stem": "string"
+    }
+  },
+  "quotes": {
+    "type": "page",
+    "fields": {
+      "id": "string",
+      "title": "string",
+      "body": "json",
+      "description": "string",
+      "extension": "string",
+      "meta": "json",
+      "navigation": "json",
+      "path": "string",
+      "seo": "json",
+      "stem": "string"
+    }
+  },
+  "memes": {
+    "type": "page",
+    "fields": {
+      "id": "string",
+      "title": "string",
+      "body": "json",
+      "description": "string",
+      "extension": "string",
+      "meta": "json",
+      "navigation": "json",
+      "path": "string",
+      "seo": "json",
+      "stem": "string"
+    }
+  },
+  "info": {
+    "type": "data",
+    "fields": {}
+  }
+}
