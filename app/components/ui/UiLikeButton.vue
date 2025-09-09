a6e8a2a2c656ad6651ed5fa02491f1fb010aeab2
Drew Harper <druharper@gmail.com>
2025-09-08 21:14:36 -0500

    save

--- app/components/ui/UiLikeButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- Compatibility wrapper: forwards all props/listeners to LikeButton -->
-  <LikeButton v-bind="$attrs" />
-</template>
-
-<script setup>
-// No props/emits; rely on fallthrough to forward to inner component
-</script>
