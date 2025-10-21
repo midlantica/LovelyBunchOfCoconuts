ae538032d337c590a96659c1d108c4b80d3e5c7e
Drew Harper <druharper@gmail.com>
2025-10-21 18:22:29 -0500

    save

---
+++ types/global-components.d.ts
@@ -0,0 +1,5 @@
+// Intentionally left blank to avoid hijacking editor navigation.
+// Avoid declaring GlobalComponents here; it causes VS Code to jump to this file
+// instead of opening the actual .vue component source.
+
+export {}
